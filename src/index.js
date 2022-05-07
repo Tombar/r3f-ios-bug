61cd5906a99e63ae4e8466f09be2c0ffc95bff4f
Martin Loy <martinloy.uy@gmail.com>
2022-05-07 11:18:28 -0300

    three r136 not working

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import './styles.css'
 
 import React, { useRef } from 'react'
-import { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client'
 
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import { EffectComposer } from '@react-three/postprocessing'
 
-
-const container = document.getElementById('root');
-const root = createRoot(container); 
+const container = document.getElementById('root')
+const root = createRoot(container)
 root.render(
   <Canvas style={{ background: '#272730' }}>
     <OrbitControls />
@@ -17,6 +16,9 @@ root.render(
       <boxBufferGeometry attach="geometry" args={[2, 2, 2]} />
       <meshNormalMaterial attach="material" />
     </mesh>
+    {/* this doesnt work */}
     <EffectComposer multisampling={8} autoClear={false}></EffectComposer>
+    {/* this work */}
+    {/* <EffectComposer multisampling={8} autoClear={true}></EffectComposer> */}
   </Canvas>
 )
